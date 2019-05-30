aeab4b1af8d82b590ff2bcb5750c9bd8c5bc7b90
Narubed Kaewserd <40911777+Narubed@users.noreply.github.com>
2019-05-30 16:20:59 +0700

    installNext

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    jsx-a11y/click-events-have-key-events: "off",
+        jsx-a11y/no-static-element-interactions: "off",
+        react/prefer-stateless-function: "off",
+        semi: "off"
+  },
+};
