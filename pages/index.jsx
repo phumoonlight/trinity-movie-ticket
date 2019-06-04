9b58a3c88edb70cf6579d890f1439f5eb196638a
Phusan Moonmuang <phumoonlight@hotmail.com>
2019-06-04 10:07:06 +0700

    install ant design

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { DatePicker } from 'antd'
+import 'antd/dist/antd.css';
 import Header from '../components/header/Header'
+
 
 function Home() {
   return (
     <div>
       <Header />
       <h1>Home</h1>
+      <DatePicker />
     </div>
   )
 }
