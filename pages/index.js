f1fd7e4a3d2104cac1256dcc5e298ebf6155f3da
Phusan Moonmuang <phumoonlight@hotmail.com>
2019-06-07 17:19:50 +0700

    minor-fix

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Header from '../src/components/layout/Header'
-import MoviesContainer from '../src/components/layout/MoviesContainer'
 import SearchBar from '../src/components/common/SearchBar'
+import MoviesContainer from '../src/components/layout/MoviesContainer'
 import Footer from '../src/components/layout/Footer'
 import 'antd/dist/antd.css'
 
-function Index(props) {
+const Index = (props) => {
   const { searchkey } = props
   return (
     <div>
