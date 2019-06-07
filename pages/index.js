fb3867f2cdc80b9f9bdb722b465b257fe3a835ac
Phusan Moonmuang <phumoonlight@hotmail.com>
2019-06-07 13:22:46 +0700

    fix header searchbar as child of header

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Header from '../src/components/layout/Header'
 import MoviesContainer from '../src/components/layout/MoviesContainer'
+import SearchBar from '../src/components/common/SearchBar'
 import 'antd/dist/antd.css'
 
 function Index(props) {
   const { searchkey } = props
   return (
     <div>
-      <Header />
+      <Header>
+        <SearchBar />
+      </Header>
       <MoviesContainer searchKey={searchkey} />
     </div>
   )
